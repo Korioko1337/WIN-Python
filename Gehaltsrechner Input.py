b6beb77513ab40e2f9e4bc35a3a15f5112441790
Korioko <annamaria.reinicke@gmail.com>
2025-09-30 21:07:54 +0200

    Gehaltsrechner User Input

---
+++ Gehaltsrechner Input.py
@@ -0,0 +1,12 @@
+stundenlohn = int(input("Gib deinen Stundenlohn ein: ",)) #eingabe stundenlohn
+
+arbeitsstunden = int(input("Gib deine Arbeitsstunden an: ",)) #eingabe arbeitsstunden
+
+verdienst = stundenlohn*arbeitsstunden #ausrechnen verdienst
+
+
+print("Arbeitsstunden: " , arbeitsstunden)
+
+print("Stundenlohn: " , stundenlohn)
+
+print("Verdienst: " , verdienst)
