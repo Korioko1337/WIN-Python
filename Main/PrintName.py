82c0091107488d4a758694fe62bf3c9790807ccd
Korioko <annamaria.reinicke@gmail.com>
2025-10-01 22:15:14 +0200

    Update

--- Main/PrintName.py
+++ Main/PrintName.py
@@ -1,25 +1,25 @@
 # Mehrfaches Ausgeben eines Namens auf verschiedene Arten
 #ausgabe des Namens 3 mal hintereinander
-print("Annamaria Reinicke")
-print("Annamaria Reinicke")
-print("Annamaria Reinicke")
+print("Vorname Nachname")
+print("Vorname Nachname")
+print("Vorname Nachname")
 
 #eine zeile, 3 mal hintereinander
-print("Annamaria Reinicke,", "Annamaria Reinicke,", "Annamaria Reinicke")
+print("Vorname Nachname,", "Vorname Nachname,", "Vorname Nachname")
 
 #string multiplikation
-f = "Annamaria Reinicke "
+f = "Vorname Nachname "
 print(f*3)
 
 #for schleife
 for i in range(3):
-    print("Annamaria Reinicke")
+    print("Vorname Nachname")
 
 #while schleife    
 a = 0
 while a < 3:
     a += 1
-    print("Annamaria Reinicke")
+    print("Vorname Nachname")
 
 #user input und dann 3 mal ausgeben   
 name = input("Gib deinen Namen ein:") #user input
